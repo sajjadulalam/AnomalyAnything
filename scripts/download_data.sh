e273704328213c3c94d5da8957342d24732fbd98
Sajjadul Alam <128792316+sajjadulalam@users.noreply.github.com>
2025-12-28 10:21:03 +0100

    Create download_data.sh

---
+++ scripts/download_data.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Download datasets manually due to licensing."
+echo "Expected paths:"
+echo "  data/mvtec/<class>/train/good/*.png"
+echo "  data/mvtec/<class>/test/good/*.png"
+echo "  data/mvtec/<class>/test/<anomaly>/*.png"
+echo "  data/visa/<class>/train/good/*.png"
+echo "  data/visa/<class>/test/good/*.png"
+echo "  data/visa/<class>/test/<anomaly>/*.png"
