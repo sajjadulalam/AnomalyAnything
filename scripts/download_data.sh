9fbba9f668f1b6feaf8db443c455ce4d609697ce
Sajjadul Alam <128792316+sajjadulalam@users.noreply.github.com>
2026-01-09 21:49:22 +0100

    Delete scripts directory

--- scripts/download_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Download datasets manually due to licensing."
-echo "Expected paths:"
-echo "  data/mvtec/<class>/train/good/*.png"
-echo "  data/mvtec/<class>/test/good/*.png"
-echo "  data/mvtec/<class>/test/<anomaly>/*.png"
-echo "  data/visa/<class>/train/good/*.png"
-echo "  data/visa/<class>/test/good/*.png"
-echo "  data/visa/<class>/test/<anomaly>/*.png"
